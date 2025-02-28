68188729fd13067ea4f20ed9258da65e60ccad96
wyf76 <yufanweng76@gmail.com>
2025-02-28 15:59:37 -0800

    yes

--- src/scenes/Title.js
+++ src/scenes/Title.js
@@ -16,7 +16,7 @@ class Title extends Phaser.Scene {
         this.load.image('minerva', 'img/minerva.png')
         this.load.image('jove', 'img/jove.png')
         this.load.image('neptune', 'img/neptune.png')
-        this.load.image('nezha', 'img/nezha.png') //added
+        this.load.image('nezha', 'img/Nezha.png') //added
 
         // load bitmap font
         this.load.bitmapFont('gem_font', 'font/gem.png', 'font/gem.xml')
